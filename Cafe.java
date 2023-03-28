1bdb330c051777c184e337efe46222a4bfbefc58
Linh Pham <lpham@smith.edu>
2023-03-28 18:50:34 -0400

    initial commits

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,46 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
 
-    public Cafe() {
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
         System.out.println("You have built a cafe: ☕");
+        this.nCoffeeOunces = 15;
+        this.nSugarPackets = 15;
+        this.nCreams = 15;
+        this.nCups = 20;
     }
     
+    /* Selling coffee method*/
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (this.nCoffeeOunces < size || this.nSugarPackets < nSugarPackets || this.nCreams < nCreams || this.nCups < 1) {
+            restock(size, nSugarPackets, nCreams, 1);
+            System.out.println("Restock needed! Please wait a minute.");
+        }
+        this.nCoffeeOunces -= size;
+        this.nSugarPackets -= nSugarPackets;
+        this.nCreams -= nCreams;
+        this.nCups -= 1;
+        System.out.println("Here's your order! Enjoy.");
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        this.nCoffeeOunces += nCoffeeOunces*10;
+        this.nSugarPackets += nSugarPackets*10;
+        this.nCreams += nCreams*10;
+        this.nCups += nCups*10;
+    }
+
     public static void main(String[] args) {
-        new Cafe();
+        Cafe campusCafe = new Cafe("Campus Cafe", "100 Green Street Northampton, MA 01063", 1);
+        System.out.println(campusCafe);
+        campusCafe.sellCoffee(15, 6, 9);
+        campusCafe.sellCoffee(15, 6, 9);
+        campusCafe.sellCoffee(15, 6, 9);
     }
     
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,103 @@
+import java.util.Hashtable;
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building {
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
-  
-    public static void main(String[] args) {
-      new Library();
-    }
-  
+  private Hashtable<String, Boolean> collection;
+
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    System.out.println("You have built a library: 📚");
+    this.collection = new Hashtable<String, Boolean>();
   }
+
+  public boolean containsTitle(String title){
+    if (this.collection.containsKey(title)){
+      return true;
+    } else{
+      return false;
+    }
+
+  } // returns true if the title appears as a key in the Libary's collection, false otherwise
+
+  public boolean isAvailable(String title){
+    if (this.collection.get(title) == true){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public void addTitle(String title){
+    if (this.containsTitle(title) == true){
+      throw new RuntimeException(title + " is already in the collection.");
+    }
+    else {
+      this.collection.put(title, true);
+    }
+  }
+  
+  public String removeTitle(String title){
+    if (this.containsTitle(title) != true){
+      throw new RuntimeException(title + " is not in the collection.");
+    } else {
+      this.collection.remove(title);
+      System.out.println("Successfully removed" + title + "from the collection!");
+      return title;
+    }
+  }
+
+  public void printCollection() {
+    System.out.println("Library collection:");
+    if (this.collection.isEmpty()){
+      System.out.println("There's no book in this collection yet.");
+    }
+    else {
+    for (String title : collection.keySet()) {
+        Boolean status = collection.get(title);
+        String availability = (status == null ? "unknown" : (status ? "available" : "checked out"));
+        System.out.println("- " + title + " (" + availability + ")");
+    }
+  }
+}
+
+  public void checkOut(String title) {
+    if (this.containsTitle(title) != true) {
+      throw new RuntimeException(title + " is not in the collection.");
+    }
+    else{
+    collection.replace(title, false);
+    System.out.println("Successfully checked out" + " " + title);
+    }
+  }
+
+  public void returnBook(String title) {
+    if (this.containsTitle(title) != true) {
+      throw new RuntimeException(title + " is not in the collection.");
+    }
+    else {
+    collection.replace(title, true);
+    }
+  }
+
+  public static void main(String[] args) {
+    Library Neilson = new Library("Neilson Library", "100 Green Street Northampton, MA 01063", 4);
+    System.out.println(Neilson);
+    Neilson.printCollection();
+    Neilson.addTitle("Gone With The Wind");
+    Neilson.addTitle("1984");
+    Neilson.addTitle("The Great Gatsby");
+    Neilson.addTitle("The Hobbit");
+    Neilson.addTitle("The Da Vinci Code");
+    Neilson.printCollection();
+    try {
+      Neilson.checkOut("ABCD");
+    } catch (Exception e) {
+      System.out.println(e); // Out of fuel
+    }
+    Neilson.checkOut("The Da Vinci Code");
+    Neilson.printCollection();
+
+  }
+}
+
